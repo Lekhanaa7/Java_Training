70f2515649cf119b7a44a560c3d29703051aa8e8
Lekhana <Lekhana@DESKTOP-AMKNNRC>
2024-02-02 21:40:23 +0530

    initial commit

---
+++ src/training/sorting.java
@@ -0,0 +1,40 @@
+package training;
+
+public class sorting { 
+		void sort(int arr[]) 
+		{ 
+			int n = arr.length; 
+
+			for (int i = 0; i < n - 1; i++) { 
+				// Find the minimum element in unsorted array 
+				int min_idx = i; 
+				for (int j = i + 1; j < n; j++) { 
+					if (arr[j] < arr[min_idx]) 
+						min_idx = j; 
+				} 
+
+				// Swap the found minimum element with the first 
+				// element 
+				int temp = arr[min_idx]; 
+				arr[min_idx] = arr[i]; 
+				arr[i] = temp; 
+			} 
+		
+			for (int i = 0; i < n; ++i) 
+				System.out.print(arr[i] + " "); 
+			System.out.println(); 
+		
+}
+
+		// main function 
+		public static void main(String args[]) 
+		{ 
+			sorting ob = new sorting(); 
+			int arr[] = { 64, 25, 12, 22, 11 }; 
+
+		
+			System.out.println("Sorted array"); 
+			ob.sort(arr);
+		} 
+}
+	
